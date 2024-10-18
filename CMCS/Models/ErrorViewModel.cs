985a6a38566cb94e9dc0d413174b9e2d5d0bcd19
ST10359034 <129750307+ST10359034@users.noreply.github.com>
2024-10-18 21:47:37 +0200

    Delete CMCS directory

--- CMCS/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CMCS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
